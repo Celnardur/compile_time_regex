328f7c69eda3beae7233b51b93aae0aa77d4e1b6
Aaron Dorrance <celnardur@protonmail.com>
2020-02-18 12:10:05 -0500

    Removed unneeded error code

--- lime_lex/src/lib.rs
+++ lime_lex/src/lib.rs
@@ -1,14 +1,12 @@
-pub mod nfa;
-//pub use nfa::NFA;
+//pub mod nfa;
 pub mod regex;
+
+//pub use nfa::NFA;
 
 
 #[derive(Debug)]
 pub struct Error {
     message: String, 
-    code: Option<String>, 
-    line: u64,
-    range: Option<(u32, u32)>,
 }
 
 impl Error {
@@ -21,38 +19,8 @@ impl Error {
     pub fn new(message: &str) -> Error {
         Error {
             message: String::from(message),
-            code: None,
-            line: 0,
-            range: None,
         }
     }
-
-    pub fn new_line(message: &str, code: &str, line: u64) -> Error {
-        Error {
-            message: String::from(message),
-            code: Some(String::from(code)),
-            line,
-            range: None, 
-        }
-    }
-
-    pub fn new_hl(message: &str, code: &str, line: u64, start: u32, end: u32) -> Error {
-        Error {
-            message: String::from(message),
-            code: Some(String::from(code)),
-            line,
-            range: Some((start, end)),
-        }
-    }
-
-    pub fn message(&self) -> &str {
-        self.message()
-    }
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
 
     pub fn message(&self) -> &str {
         self.message()
